d2751f285f5760cbe5b24554beec04cf794e0b3d
Li Edwin <eli@.et.esiea.fr>
2022-02-18 13:03:11 +0100

    Sample commit

---
+++ Syracuse.py
@@ -0,0 +1,18 @@
+def syracuse(n, count):
+    if n > 1:
+        print("Etape: {}".format(count))
+        print("=> {}".format(n))
+        if(n%2==0):
+            print("Paire => {}/2".format(n))
+        else:
+            print("Impaire => 3*{}+1".format(n))
+        print ("--------------------------------------------")
+        return syracuse(3 * n + 1 if n % 2 == 1 else int(n / 2), count + 1)
+    print("Resultat is :")
+    return n
+
+
+
+
+
+print(syracuse(45, 1))
